27e1ea9f551162f9aefccefd27b723c9ac365142
Tomas Jais <tomijais@gmail.com>
2022-09-17 15:55:10 -0300

    feat: upload smart contract erc20

---
+++ contracts/ERC-20.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.7.3/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.7.3/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.7.3/security/Pausable.sol";
+import "@openzeppelin/contracts@4.7.3/access/Ownable.sol";
+
+contract PAINT is ERC20, ERC20Burnable, Pausable, Ownable {
+    constructor() ERC20("PAINT", "PAINT") {
+        _mint(msg.sender, 100 * 10 ** decimals());
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+        /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5.05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei. This is the value {ERC20} uses, unless this function is
+     * overridden;
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {IERC20-balanceOf} and {IERC20-transfer}.
+     */
+    function decimals() public view virtual override returns (uint8) {
+        return 0;
+    }
+}
